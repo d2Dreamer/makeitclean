2a0ac326b78175c6b7f568100d30313a25e6d3ee
d2Dreamer <41088824+d2Dreamer@users.noreply.github.com>
2022-03-21 18:24:27 +0000

    Added Navigation page and button to route to it on mainactivity

--- app/src/main/java/com/example/makeitclean/MainActivity.java
+++ app/src/main/java/com/example/makeitclean/MainActivity.java
@@ -5,15 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    Button mNavigationBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNavigationBtn = findViewById(R.id.navigationBtn);
+
+        mNavigationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Navigation.class));
+            }
+        });
     }
 
     public void logout(View view) {
