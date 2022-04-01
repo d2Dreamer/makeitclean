38a8beaae943e44608aea2f92f8a8d4008dfd357
d2Dreamer <41088824+d2Dreamer@users.noreply.github.com>
2022-04-01 13:56:28 +0100

    Added routing between Fragments using NavBar

--- app/src/main/java/com/example/makeitclean/MainActivity.java
+++ app/src/main/java/com/example/makeitclean/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.makeitclean;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.makeitclean.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new homeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+
+                case R.id.homeFragment:
+                    replaceFragment(new homeFragment());
+                    break;
+                case R.id.profileFragment:
+                    replaceFragment(new profileFragment());
+                    break;
+                case R.id.navigationFragment:
+                    replaceFragment(new navigationFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
     }
 }
