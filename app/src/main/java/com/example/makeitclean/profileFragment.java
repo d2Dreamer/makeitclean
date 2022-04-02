6872c199bb8b5b53c45b7800fcdfd383132672bc
d2Dreamer <41088824+d2Dreamer@users.noreply.github.com>
2022-04-02 14:07:41 +0100

    fixed logout

--- app/src/main/java/com/example/makeitclean/profileFragment.java
+++ app/src/main/java/com/example/makeitclean/profileFragment.java
@@ -5,16 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class profileFragment extends Fragment {
-
-    Button mNavigationBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,14 +19,6 @@ public class profileFragment extends Fragment {
         //Initialize view
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        mNavigationBtn = getView().findViewById(R.id.navigationBtn);
-
-        mNavigationBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getActivity(), Navigation.class));
-            }
-        });
         return view;
     }
 
